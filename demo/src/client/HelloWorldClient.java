500b00e112ff30d335285a2245fce51ba094e70e
Shreya Patidar <spatidar@BLR.ALLEGISINDIA.COM>
2022-04-21 11:23:35 +0530

    'Hibernate'

---
+++ demo/src/client/HelloWorldClient.java
@@ -0,0 +1,22 @@
+package client;
+
+import org.hibernate.Session;
+
+import domain.Message;
+import util.HIbernateUtil;
+
+public class HelloWorldClient {
+
+	public static void main(String[] args) {
+		
+		Session session = HIbernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		
+		Message message = new Message("Hello  from annotations");
+				
+		session.save(message);
+		
+		session.getTransaction().commit();
+		session.close();
+	}
+}
